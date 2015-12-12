29b1e6bcec3be5dcf3ecef0f48b4ad396a390eeb
nelson8323 <nelson5326@gmail.com>
2015-12-12 16:26:53 +0800

    app

---
+++ api/controllers/CommentController.js
@@ -0,0 +1,10 @@
+/**
+ * CommentController
+ *
+ * @description :: Server-side logic for managing comments
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
